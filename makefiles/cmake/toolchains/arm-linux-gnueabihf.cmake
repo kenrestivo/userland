7a970d78e2e33efa9bcb03eb6b70ed55e2b2b40c
ken restivo <ken@restivo.org>
2015-07-05 23:38:38 -0700

    Make this compile with Debian Jesse crosscompiler.

--- makefiles/cmake/toolchains/arm-linux-gnueabihf.cmake
+++ makefiles/cmake/toolchains/arm-linux-gnueabihf.cmake
@@ -10,7 +10,8 @@ SET(CMAKE_ASM_COMPILER arm-linux-gnueabihf-gcc)
 SET(CMAKE_SYSTEM_PROCESSOR arm)
 
 #ADD_DEFINITIONS("-march=armv6")
-add_definitions("-mcpu=arm1176jzf-s -mfpu=vfp -mfloat-abi=hard")
+add_definitions("-mcpu=cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=hard")
+
 
 # rdynamic means the backtrace should work
 IF (CMAKE_BUILD_TYPE MATCHES "Debug")
